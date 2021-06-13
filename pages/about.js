6bddb1ca80091215862e8f12f95662c83441efe2
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-06-13 03:43:16 +0200

    [NS-2] Use Next API feature to get data instead of External API

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Head from 'next/head';
+import Meta from "../components/Meta";
 
 const about = () => {
   return (
     <div>
-      <Head>
-        <title>About</title>
-        <meta name="keywords" content="About page, web development, programming" />
-      </Head>
+      <Meta title='About' />
       <h1>About Page</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
-import Head from 'next/head'
 import ArticleList from '../components/ArticleList';
+import { server } from '../config';
+
 
 export default function Home({articles}) {
   console.log(articles);
   return (
     <div>
-      <Head>
-        <title>WebDev NewZ</title>
-        <meta name='keywords' content='web development, programming' />
-      </Head>
       <ArticleList articles={articles} />
     </div>
   )
 }
 
+/**
+ * 
+ * @returns Local API data.json
+ */
 export const getStaticProps = async() => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return  {
@@ -24,3 +25,17 @@ export const getStaticProps = async() => {
     }
   }
 }
+
+/**
+ * External API
+ */
+// export const getStaticProps = async() => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return  {
+//     props: {
+//       articles
+//     }
+//   }
+// }
